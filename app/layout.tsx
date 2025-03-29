0630c0e3ce8420df4bcff4809ff188389d8368dc
Hammam Firdaus <hammamxalf@gmail.com>
2025-03-29 11:27:24 +0700

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,25 @@ export const metadata: Metadata = {
   title: "Wedding Invitation",
   description: "Wedding Invitation",
   generator: "FPK Creative",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        type: "image/ico",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
